04a12759d3a23afe9568a8ecebf41f627ff41770
Cristhian Jimenez <cristhian.jimenez@iatec.com>
2022-03-11 14:03:24 -0400

    Styles were changed

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
           <a href="https://github.com/cjimenez715">
             <div className="dsmovie-contact-container">
               <GitHubIcon />
-              <p className="dsmovie-contact-link">/devsuperior</p>
+              <p className="dsmovie-contact-link">/cjimenez715</p>
             </div>
           </a>
         </div>

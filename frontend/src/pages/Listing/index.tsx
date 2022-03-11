d4b4284e6fcee47575bba2429e17d4c68170e427
Cristhian Jimenez <cristhian.jimenez@iatec.com>
2022-03-11 12:26:52 -0400

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,37 +7,40 @@ import { MoviePage } from 'types/movie';
 
 function Listing() {
   const [pageNumber, setPageNumber] = useState(0);
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 0,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=2`).then(res => {
-      console.log(res.data);
-      const data = res.data as MoviePage;
-      setPageNumber(data.number);
-    });
-  }, []);
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then(res => {
+        const data = res.data as MoviePage;
+        setPage(data);
+      });
+  }, [pageNumber]);
 
   return (
     <>
-      <p>{pageNumber}</p>
       <Pagination />
 
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
+          {page.content.map(movie => {
+            return (
+              <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                <MovieCard key={movie.id} movie={movie} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
